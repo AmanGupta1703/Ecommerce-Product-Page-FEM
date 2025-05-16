5c5b85ce169759d0b31309aa77c40c0cc27cfe6e
Kunal Gupta <guptakunalx@gmail.com>
2025-05-16 16:46:25 +0530

    feat: add Button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,21 @@
+import styles from "./Button.module.css";
+
+type TButtonTypes = "primary";
+type TButtonClassnames = "btn-cart";
+
+type TButtonProps = {
+  type?: TButtonTypes;
+  className: TButtonClassnames;
+
+  children: React.ReactNode;
+};
+
+const Button = ({ type = "primary", className, children }: TButtonProps) => {
+  return (
+    <button className={`${styles.btn} ${styles[className]}  ${styles[`btn-${type}`]}`}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
